9ebbd177d43adb32dd7127879d0f54830eb47552
Ashutosh Dhondi <dhondiashutoshvarma@gmail.com>
2025-07-10 14:10:11 -0400

    init commit of Builder design pattern

--- src/com/designpatterns/builder/Shop.java
+++ src/com/designpatterns/builder/Shop.java
@@ -2,7 +2,7 @@ package com.designpatterns.builder;
 
 public class Shop {
     public static void main(String[] args) {
-        Phone phone = new PhoneBuilder().setOs("android").setRam(2).getPhone();
+        Phone phone = new PhoneBuilder().setOs("android").setRam(2).setProcessor("Mediatek").getPhone();
         System.out.println(phone);
     }
 }
